77fff71412b7c2e3716b76228dceac2c1e68bf2c
Jougo Cahya Wirana <jugowirana1@gmail.com>
2024-08-25 16:18:59 +0700

    Add project files.

---
+++ PrintPreviews/MainWindow.xaml.vb
@@ -0,0 +1,24 @@
+﻿Class MainWindow
+    Private Sub PrintPreviewButton_Click(sender As Object, e As RoutedEventArgs)
+        Try
+            Dim printdialog As New PrintDialog()
+            If printdialog.ShowDialog() = True Then
+                ' Tentukan ukuran kertas custom dalam inci
+                'Dim widthInInches As Double = 8.5
+                'Dim heightInInches As Double = 11
+
+                ' Konversi ukuran kertas ke piksel (1 inci = 96 piksel)
+                'Dim widthInPixels As Double = widthInInches * 96
+                'Dim heightInPixels As Double = heightInInches * 96
+
+                ' Atur ukuran kertas pada PrintTicket
+                'printdialog.PrintTicket.PageMediaSize = New System.Printing.PageMediaSize(widthInPixels, heightInPixels)
+
+                ' Cetak visual
+                printdialog.PrintVisual(AreaPrint, "AreaPrint")
+            End If
+        Catch ex As Exception
+            ' Tambahkan penanganan kesalahan di sini jika diperlukan
+        End Try
+    End Sub
+End Class
